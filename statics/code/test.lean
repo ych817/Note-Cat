8d377341bf2f828930c9b6f5fcb5a17de7cd5c86
ych817 <ych817@aucklanduni.ac.nz>
2026-03-20 10:22:31 +1300

    小修补

--- statics/code/test.lean
+++ statics/code/test.lean
@@ -1,13 +1,16 @@
-variable
-(α : Type)
-(p q : α → Prop)
-(r : Prop)
+variable {p q r : Prop}
 
-example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x)
+example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r)
   :=Iff.intro
-    (λ (h : ∃ x, p x ∨ q x) ↦ match h with
-    | ⟨x, Or.inl hp⟩ => Or.inl ⟨x, hp⟩
-    | ⟨x, Or.inr hq⟩ => Or.inr ⟨x, hq⟩)
-    (λ (h : (∃ x, p x) ∨ (∃ x, q x)) ↦ match h with
-    | Or.inl ⟨x, hp⟩ => ⟨x, Or.inl hp⟩
-    | Or.inr ⟨x, hq⟩ => ⟨x, Or.inr hq⟩)
+    (λ ⟨⟨hp, hq⟩, hr⟩ => ⟨hp, ⟨hq, hr⟩⟩)
+    (λ ⟨hp, ⟨hq, hr⟩⟩ => ⟨⟨hp, hq⟩, hr⟩)
+
+example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r)
+  :=Iff.intro
+    (λ
+      | Or.inl hp => ⟨Or.inl hp, Or.inl hp⟩
+      | Or.inr ⟨hq, hr⟩ => ⟨Or.inr hq, Or.inr hr⟩)
+    (λ
+      | ⟨Or.inl hp, _⟩ => Or.inl hp
+      | ⟨_, Or.inl hp⟩ => Or.inl hp
+      | ⟨Or.inr hq, Or.inr hr⟩ => Or.inr ⟨hq, hr⟩)
